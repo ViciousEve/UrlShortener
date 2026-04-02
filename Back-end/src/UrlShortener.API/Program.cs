190962e616f3a28bc95e8cee620575b61407de22
ViciousEve <146147992+ViciousEve@users.noreply.github.com>
2026-04-02 13:35:33 +0700

    added exception handler, update package props, added dockerfile for container building

--- Back-end/src/UrlShortener.API/Program.cs
+++ Back-end/src/UrlShortener.API/Program.cs
@@ -1,6 +1,36 @@
+using Analytics;
+using Shortening;
+using UrlShortener.API.ExceptionHandlers;
+using Shortening.Presentation;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Exception handlers
+builder.Services.AddExceptionHandler<ValidationExceptionHandler>();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add modules
+builder.Services.AddShorteningModule(builder.Configuration);
+builder.Services.AddAnalyticsModule(builder.Configuration);
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler();
+
+app.MapShorteningModule();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
